c99a2c835b8055b50b0f44487ca64deb881964b1
Michael Gruener <michael.gruener@chaosmoon.net>
2014-10-27 01:41:51 +0100

    Fix handling of the default value of the installdestdir parameter
    puppet does not allow one parameter being the default value of a
    different parameter

--- manifests/install/staging.pp
+++ manifests/install/staging.pp
@@ -1,21 +1,27 @@
 class gitblit::install::staging (
   $version    = '1.6.1',
-  $installdestdir = "/opt/gitblit-${version}",
+  $installdestdir = undef,
 ) {
+
+  # crude hack because puppet does not allow a class
+  # parameter to be used as default value for another
+  # class parameter
+  $installdestdir_real = pick($installdestdir,"/opt/gitblit-${version}")
+
   $filename     = "gitblit-${version}.tar.gz"
   $download_url = "http://dl.bintray.com/gitblit/releases/${filename}"
 
   staging::deploy { $filename:
     source => $download_url,
-    target => $installdestdir,
+    target => $installdestdir_real,
   }
 
   file { $gitblit::installdir:
     ensure => link,
-    target => $installdestdir,
+    target => $installdestdir_real,
   }
 
-  exec { "cp -r ${installdestdir}/data/* ${gitblit::datadir}/":
+  exec { "cp -r ${installdestdir_real}/data/* ${gitblit::datadir}/":
     path        => '/bin:/usr/bin',
     creates     => "${gitblit::datadir}/gitblit.properties",
     refreshonly => true,
