d33f79314da2b8499ac48273c0aebacc82b10783
Michael Gruener <michael.gruener@chaosmoon.net>
2015-01-10 15:02:21 +0100

    Make user/team/project management through puppet optional

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,26 +1,28 @@
 class gitblit (
-  $datadir           = '/var/lib/gitblit',
-  $installdir        = '/opt/gitblit',
-  $http_port         = '0',
-  $https_port        = '8443',
-  $user              = 'gitblit',
-  $group             = 'gitblit',
-  $manageuser        = true,
-  $service_name      = $gitblit::params::service_name,
-  $service_provider  = $gitblit::params::service_provider,
-  $service_template  = $gitblit::params::service_template,
-  $service_path      = $gitblit::params::service_path,
-  $service_config    = $gitblit::params::service_config,
-  $service_ensure    = running,
-  $service_enable    = true,
-  $adminpassword     = 'admin',
-  $adminpasswordtype = 'combined-md5',
-  $mainprojectdoc    = undef,
-  $config            = {},
-  $users             = {},
-  $teams             = {},
-  $projects          = {},
-  $hiera_merge       = false,
+  $datadir                 = '/var/lib/gitblit',
+  $installdir              = '/opt/gitblit',
+  $http_port               = '0',
+  $https_port              = '8443',
+  $user                    = 'gitblit',
+  $group                   = 'gitblit',
+  $manageuser              = true,
+  $manage_gitblit_user     = true,
+  $manage_gitblit_projects = true,
+  $service_name            = $gitblit::params::service_name,
+  $service_provider        = $gitblit::params::service_provider,
+  $service_template        = $gitblit::params::service_template,
+  $service_path            = $gitblit::params::service_path,
+  $service_config          = $gitblit::params::service_config,
+  $service_ensure          = running,
+  $service_enable          = true,
+  $adminpassword           = 'admin',
+  $adminpasswordtype       = 'combined-md5',
+  $mainprojectdoc          = undef,
+  $config                  = {},
+  $users                   = {},
+  $teams                   = {},
+  $projects                = {},
+  $hiera_merge             = false,
 ) inherits gitblit::params {
   # only one installation method at the moment
   include gitblit::install::staging
@@ -55,41 +57,50 @@ class gitblit (
     require  => [File[$service_path],File[$service_config]],
   }
 
-  concat { 'users.conf':
-    ensure  => present,
-    path    => "${datadir}/users.conf",
-    owner   => $user,
-    group   => $group,
-    mode    => '0640',
-    warn    => true,
-    replace => true,
-    require => File[$datadir],
+  if $manage_gitblit_users {
+    concat { 'users.conf':
+      ensure  => present,
+      path    => "${datadir}/users.conf",
+      owner   => $user,
+      group   => $group,
+      mode    => '0640',
+      warn    => true,
+      replace => true,
+      require => File[$datadir],
+    }
+
+    gitblit::user { 'admin':
+      ensure       => present,
+      passwordtype => $adminpasswordtype,
+      password     => $adminpassword,
+      roles        => [ '#admin', '#notfederated' ],
+      accounttype  => 'LOCAL',
+    }
+
+    create_resources('gitblit::user',$users_real)
+    create_resources('gitblit::team',$teams_real)
   }
 
-  concat { 'projects.conf':
-    ensure  => present,
-    path    => "${datadir}/projects.conf",
-    owner   => $user,
-    group   => $group,
-    mode    => '0644',
-    warn    => true,
-    replace => true,
-    require => File[$datadir],
-  }
+  if $manage_gitblit_projects {
+    concat { 'projects.conf':
+      ensure  => present,
+      path    => "${datadir}/projects.conf",
+      owner   => $user,
+      group   => $group,
+      mode    => '0644',
+      warn    => true,
+      replace => true,
+      require => File[$datadir],
+    }
 
-  gitblit::project { 'main':
-    ensure      => present,
-    name        => 'Main Repositories',
-    description => 'main group of repositories',
-    doc         => $mainprojectdoc
-  }
+    gitblit::project { 'main':
+      ensure      => present,
+      name        => 'Main Repositories',
+      description => 'main group of repositories',
+      doc         => $mainprojectdoc
+    }
 
-  gitblit::user { 'admin':
-    ensure       => present,
-    passwordtype => $adminpasswordtype,
-    password     => $adminpassword,
-    roles        => [ '#admin', '#notfederated' ],
-    accounttype  => 'LOCAL',
+    create_resources('gitblit::project',$projects_real)
   }
 
   if $manageuser {
@@ -121,7 +132,4 @@ class gitblit (
   }
 
   create_resources('gitblit::config',$config_real)
-  create_resources('gitblit::user',$users_real)
-  create_resources('gitblit::team',$teams_real)
-  create_resources('gitblit::project',$projects_real)
 }
