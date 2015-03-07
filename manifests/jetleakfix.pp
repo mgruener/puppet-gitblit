aa279ccd1e99c2bedfc000557ce8b4a770308656
Michael Gruener <michael.gruener@cloudlynx.ch>
2015-03-07 18:17:38 +0100

    Add quick class for JetLeak (CVE-2015-2080)

---
+++ manifests/jetleakfix.pp
@@ -0,0 +1,25 @@
+# Quickfix for
+# https://github.com/eclipse/jetty.project/blob/master/advisories/2015-02-24-httpparser-error-buffer-bleed.md
+class gitblit::jetleakfix (
+  $version = '9.2.9.v20150224',
+  $baseurl = 'http://repo1.maven.org/maven2/org/eclipse/jetty/aggregate/jetty-all',
+  $absoluteurl = '',
+) {
+  require gitblit
+
+  $file = "jetty-all-${version}.jar"
+  if $absoluteurl {
+    $url = $absoluteurl
+  } else {
+    $url = "${baseurl}/${file}"
+  }
+
+  staging::file { $file:
+    source => "${url}",
+    target => "${::gitblit::installdir}/ext/${file}",
+  }
+
+  file { "${::gitblit::installdir}/ext/jetty-all-9.2.3.v20140905.jar":
+    ensure => absent
+  }
+}
