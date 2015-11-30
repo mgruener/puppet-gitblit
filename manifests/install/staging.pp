7e3f84e5d26dc5bfae327125541d4200b0cdab51
Michael Gruener <michael.gruener@bedag.ch>
2015-11-30 22:19:34 +0100

    Fixed #6 - Make download_url a class parameter

--- manifests/install/staging.pp
+++ manifests/install/staging.pp
@@ -1,18 +1,18 @@
 class gitblit::install::staging (
   $version    = '1.6.1',
   $installdestdir = undef,
+  $download_url   = undef
 ) {
 
-  # crude hack because puppet does not allow a class
-  # parameter to be used as default value for another
-  # class parameter
+  # crude hack because puppet does not allow (reliably) a class
+  # parameter to be used as default value for another class parameter
+  # see https://tickets.puppetlabs.com/browse/PUP-1080
   $installdestdir_real = pick($installdestdir,"/opt/gitblit-${version}")
-
-  $filename     = "gitblit-${version}.tar.gz"
-  $download_url = "http://dl.bintray.com/gitblit/releases/${filename}"
+  $download_url_real = pick($download_url,"http://dl.bintray.com/gitblit/releases/gitblit-${version}.tar.gz")
+  $filename          = basename($download_url_real)
 
   staging::deploy { $filename:
-    source  => $download_url,
+    source  => $download_url_real,
     target  => $installdestdir_real,
     creates => "${installdestdir_real}/gitblit.jar",
     require => File[$installdestdir_real],
