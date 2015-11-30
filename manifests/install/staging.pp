b2fc09d4d298ef6550da4d7642bb5e2b73e6be79
Michael Gruener <michael.gruener@bedag.ch>
2015-11-30 23:07:21 +0100

    Add support for 1.7.x source archive structure #5

--- manifests/install/staging.pp
+++ manifests/install/staging.pp
@@ -1,37 +1,43 @@
 class gitblit::install::staging (
   $version    = '1.6.1',
-  $installdestdir = undef,
+  $installdestdir = '/opt',
   $download_url   = undef
 ) {
 
   # crude hack because puppet does not allow (reliably) a class
   # parameter to be used as default value for another class parameter
   # see https://tickets.puppetlabs.com/browse/PUP-1080
-  $installdestdir_real = pick($installdestdir,"/opt/gitblit-${version}")
   $download_url_real = pick($download_url,"http://dl.bintray.com/gitblit/releases/gitblit-${version}.tar.gz")
   $filename          = basename($download_url_real)
 
-  staging::deploy { $filename:
-    source  => $download_url_real,
-    target  => $installdestdir_real,
-    creates => "${installdestdir_real}/gitblit.jar",
-    require => File[$installdestdir_real],
+  $basedir = "${installdestdir}/gitblit-${version}"
+  if versioncmp('1.6',$version) <= 0 {
+    $targetdir = $basedir
+
+    file { $targetdir:
+      ensure => directory,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0755',
+      before => Staging::Deploy[$filename],
+    }
+  } else {
+    $targetdir = $installdestdir
   }
 
-  file { $installdestdir_real:
-    ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
+  staging::deploy { $filename:
+    source  => $download_url_real,
+    target  => $targetdir,
+    creates => "${basedir}/gitblit.jar",
   }
 
   file { $gitblit::installdir:
     ensure => link,
-    target => $installdestdir_real,
+    target => $basedir,
   }
 
   exec { 'initial-datadir-provisioning':
-    command     => "cp -r ${installdestdir_real}/data/* ${gitblit::datadir}/",
+    command     => "cp -r ${basedir}/data/* ${gitblit::datadir}/",
     path        => '/bin:/usr/bin',
     creates     => "${gitblit::datadir}/certs",
     require     => Staging::Deploy[$filename],
